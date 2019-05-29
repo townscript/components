39f51fe199f56bd6f1d58f74e1ea1dfefce0a5f5
sidharth6323 <sidharth6323@gmail.com>
2019-05-29 14:30:17 +0530

    Added dist

--- dist/src/app/modules/layout/components/ts-footer/ts-footer.component.d.ts
+++ dist/src/app/modules/layout/components/ts-footer/ts-footer.component.d.ts
@@ -1,12 +1,14 @@
 import { OnInit } from '@angular/core';
 export declare class TsFooterComponent implements OnInit {
-    placeId: any;
     city: any;
+    placeId: any;
+    source: any;
+    popularEvents: any;
+    recentBlogs: any;
+    popularReads: any;
     popularEventsData: any;
     countryCityMap: any;
     constructor();
-    setFromTownscript: (value: any) => void;
-    onChangeCity: (city: any) => void;
     openContactUs: () => void;
     openMyBooking: () => void;
     ngOnInit(): void;
