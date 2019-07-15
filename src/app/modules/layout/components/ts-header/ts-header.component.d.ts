9506af5686c04fbbae164fdd4ff4542a6dbe12f8
sidharth6323 <sidharth6323@gmail.com>
2019-07-15 14:51:06 +0530

    migrated to 7

--- src/app/modules/layout/components/ts-header/ts-header.component.d.ts
+++ src/app/modules/layout/components/ts-header/ts-header.component.d.ts
@@ -1,6 +1,7 @@
 import { OnInit } from '@angular/core';
 export declare class TsHeaderComponent implements OnInit {
     Components: Array<String>;
+    source: string;
     constructor();
     ngOnInit(): void;
 }
