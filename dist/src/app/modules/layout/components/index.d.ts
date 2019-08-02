7dc85b9f03ab296f3a98a8e2fc5942ca8942a266
prabir09 <prabir.rishi@townscript.com>
2019-08-02 14:31:38 +0530

    added listing card component

--- dist/src/app/modules/layout/components/index.d.ts
+++ dist/src/app/modules/layout/components/index.d.ts
@@ -1,3 +1,4 @@
 export * from './ts-header/ts-header.component';
 export * from './ts-footer/ts-footer.component';
 export * from './ts-header/search/search.component';
+export * from './ts-listing-card/ts-listing-card.component';
--- dist/townscript-components.d.ts
+++ dist/townscript-components.d.ts
@@ -2,3 +2,4 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public_api';
+export { RangeDatePipe as ɵa } from './src/app/modules/layout/components/ts-listing-card/ts-date-range.pipe';
