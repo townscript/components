47bd979984d26d13d363e57ade5a83f54b60d0c8
sidharth6323 <sidharth6323@gmail.com>
2019-08-02 18:20:14 +0530

    Initial dist subtree commit

--- dist/src/app/modules/layout/components/ts-header/ts-header.component.d.ts
+++ dist/src/app/modules/layout/components/ts-header/ts-header.component.d.ts
@@ -1,10 +1,12 @@
 import { OnInit } from '@angular/core';
 import { DatePipe } from '@angular/common';
+import { Router } from '@angular/router';
 export declare class TsHeaderComponent implements OnInit {
     datepipe: DatePipe;
     Components: Array<String>;
     source: string;
     algoliaIndexName: string;
+    router: Router;
     constructor(datepipe: DatePipe);
     ngOnInit(): void;
 }
