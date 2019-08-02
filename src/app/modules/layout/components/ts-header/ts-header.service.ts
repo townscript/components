a18f92aa954456c398109ce07ece703a3b53ea3f
sidharth6323 <sidharth6323@gmail.com>
2019-08-02 18:14:32 +0530

    added router as input

--- src/app/modules/layout/components/ts-header/ts-header.service.ts
+++ src/app/modules/layout/components/ts-header/ts-header.service.ts
@@ -1,12 +1,14 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Input } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { config } from 'src/app/core';
+import { config } from '../../../../core';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class HeaderService {
 
     baseUrl: string = config.baseUrl;
     apiServerUrl: string = this.baseUrl + "api/";
+    @Input() router: Router;
     constructor(private http: HttpClient) {
     }
     getplaceSearchResults = (query) => {
