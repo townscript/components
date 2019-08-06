ebb2b9a9cc964b4a0e0218f1b8ddacc2dd989cc6
prabir09 <prabir.rishi@townscript.com>
2019-08-06 10:56:31 +0530

    integrate login to header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {MatDialog, MatDialogConfig} from "@angular/material";
+import {MatDialog, MatDialogConfig} from '@angular/material';
 import { TsLoginSignupComponent } from './modules/layout/components';
 
 @Component({
--- src/app/modules/layout/components/ts-header/ts-header.component.ts
+++ src/app/modules/layout/components/ts-header/ts-header.component.ts
@@ -1,8 +1,8 @@
 import { Component, Input, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
 import { TimeService } from '../../../../shared/services/time.service';
-import { DatePipe } from '@angular/common'
+import {MatDialog, MatDialogConfig} from '@angular/material';
 import { Router } from '@angular/router';
-
+import { TsLoginSignupComponent} from '../../components/ts-login-signup/ts-login-signup.component';
 
 @Component({
   selector: 'ts-header',
@@ -11,23 +11,36 @@ import { Router } from '@angular/router';
 })
 export class TsHeaderComponent implements OnInit {
 
-  @Input() Components: Array<String> = ["createEventBtn"];
-  @Input() source: string = "marketplace";
-  @Input() algoliaIndexName: string = "tsTesting";
+  @Input() Components: Array<String> = ['createEventBtn'];
+  @Input() source = 'marketplace';
+  @Input() algoliaIndexName = 'tsTesting';
   @Input() router: Router;
   @ViewChild('citySuggestions', { static: false }) citySuggestions: ElementRef;
 
-  cityPopupActive: boolean = false;
-  constructor(public datepipe: DatePipe) {
+  cityPopupActive = false;
+  constructor(private dialog: MatDialog) {
   }
 
   @HostListener('document:click', ['$event'])
+
   clickout(event) {
-    console.log("clickout called");
+    console.log('clickout called');
     if (!this.citySuggestions.nativeElement.contains(event.target)) {
       this.cityPopupActive = false;
     }
   }
+
+  openLogin(type) {
+    const dialogConfig = new MatDialogConfig();
+    console.log('in Login');
+    dialogConfig.disableClose = false;
+    dialogConfig.autoFocus = true;
+    dialogConfig.minWidth = '900px';
+    dialogConfig.minHeight = '530px';
+    dialogConfig.height = 'auto';
+    dialogConfig.backdropClass = 'mat-dialog-bkg-container';
+    this.dialog.open(TsLoginSignupComponent, dialogConfig);
+}
 
   ngOnInit() {
   }
