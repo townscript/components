2edbc23efc791124eb6246f0636562cd34ed29e5
prabir09 <prabir.rishi@townscript.com>
2019-08-14 15:51:08 +0530

    added featured Card

--- dist/src/app/modules/layout/components/ts-listing-card/ts-listing-card.component.d.ts
+++ dist/src/app/modules/layout/components/ts-listing-card/ts-listing-card.component.d.ts
@@ -1,11 +1,15 @@
 import { OnInit } from '@angular/core';
 export declare class TsListingCardComponent implements OnInit {
     eventData: any;
+    type: any;
     urgencyMessage: boolean;
     goingCounter: boolean;
+    moreIcons: boolean;
     keywords: {
         keyCode: string;
     }[];
+    showRegularCard: boolean;
+    featuredCard: boolean;
     constructor();
     ngOnInit(): void;
 }
