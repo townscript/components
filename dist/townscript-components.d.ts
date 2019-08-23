bf1f5b773530d8e94c185308ebf7c1e4f33f2959
prabir09 <prabir.rishi@townscript.com>
2019-08-23 14:09:24 +0530

    update login modal

--- dist/townscript-components.d.ts
+++ dist/townscript-components.d.ts
@@ -3,9 +3,8 @@
  */
 export * from './public_api';
 export { HeaderService as ɵb } from './src/app/modules/layout/components/ts-header/ts-header.service';
-export { UserMenuComponent as ɵf } from './src/app/modules/layout/components/ts-header/user-menu/user-menu.component';
-export { RangeDatePipe as ɵe } from './src/app/modules/layout/components/ts-listing-card/ts-date-range.pipe';
+export { UserMenuComponent as ɵe } from './src/app/modules/layout/components/ts-header/user-menu/user-menu.component';
 export { CookieService as ɵa } from './src/app/modules/layout/components/ts-login-signup/cookie.service';
 export { LoginTopContentComponent as ɵd } from './src/app/modules/layout/components/ts-login-signup/login-top-content/login-top-content.component';
-export { AppPasswordDirective as ɵg } from './src/app/modules/layout/components/ts-login-signup/ts-show-hide-directive';
+export { AppPasswordDirective as ɵf } from './src/app/modules/layout/components/ts-login-signup/ts-show-hide-directive';
 export { NotificationService as ɵc } from './src/app/shared/services/notification.service';
