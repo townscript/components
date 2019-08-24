64fd62be2da97eaf8cac58cdfd68fb2ff247a547
sidharth6323 <sidharth6323@gmail.com>
2019-08-24 07:54:07 +0530

    haar maan gya mai commit kr kr ke

--- dist/public_api.d.ts
+++ dist/public_api.d.ts
@@ -4,6 +4,7 @@ export * from './src/app/modules/layout/components/index';
 export * from './src/app/modules/layout/layout.module';
 export * from './src/app/modules/loginSignup/login-signup.module';
 export * from './src/app/modules/loginSignup/index';
+export * from './src/app/core/browser.service';
 export * from './src/app/modules/cards/ts-listing-card/ts-date-range.pipe';
 export * from './src/app/modules/cards/ts-listing-card/share-event-modal/share-event-modal.component';
 export * from './src/app/modules/cards/ts-card-skeleton/ts-card-skeleton.component';
