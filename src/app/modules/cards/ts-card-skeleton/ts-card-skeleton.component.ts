1022e2bcc257086a683b2ecf305f2dcd69be953e
sidharth6323 <sidharth6323@gmail.com>
2019-09-04 02:32:16 +0530

    made skeleton handle grid input

--- src/app/modules/cards/ts-card-skeleton/ts-card-skeleton.component.ts
+++ src/app/modules/cards/ts-card-skeleton/ts-card-skeleton.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'ts-card-skeleton',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TsCardSkeletonComponent implements OnInit {
 
+  @Input() gridType: string = "list";
   constructor() { }
 
   ngOnInit() {
