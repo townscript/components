06130c06cf5e36d981b86176732b4b4dc5b25f6f
sidharth6323 <sidharth6323@gmail.com>
2019-09-06 13:29:12 +0530

    changed all base urls

--- src/app/shared/services/api-service.ts
+++ src/app/shared/services/api-service.ts
@@ -10,10 +10,10 @@ export class ApiService {
     betaHostName = config.betaHostName || 'beta.tsdugout.in/';
     S3_BUCKET_NAME = 'townscript-testing';
     GA_TRACKER_CODE = 'UA-68181318-1';
-    SERVER_URL = 'https://www.tsdugout.in';
-    API_SERVER = 'https://www.tsdugout.in/api/';
+    SERVER_URL = config.baseUrl;
+    API_SERVER = config.baseUrl + 'api/';
     algoliaIndexName = 'tsTesting';
     IPINFO_ACCESS_TOKEN = 'a27cfbcc77e854';	// change afterwards
     RECORD_FOR_KINESIS = true; // temporary
-    PAYMENT_PAGE_URL = 'https://www.tsdugout.in/payment/';
+    PAYMENT_PAGE_URL = config.baseUrl + 'payment/';
 }
