f6665fe60c59ec35a1c1f8f8b1265773e440e033
sidharth6323 <sidharth6323@gmail.com>
2019-10-21 14:38:20 +0530

    removed dist

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+echo "Enter the commit message"
 if [ -z "$1" ]
 then
   echo "Which folder do you want to deploy to GitHub Pages?"
+  git add dist && git commit -m "$1"
+  git subtree push --prefix dist origin library-angular8--force
   exit 1
 fi
-git subtree push --prefix $1 origin library
