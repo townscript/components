edeac9373154a48d50ca50f806efac8ea19b2b4d
sumedhmohaneTS <sumedh.mohane@townscript.com>
2019-11-21 13:59:37 +0530

    remove redundant imports

--- src/app/modules/loginSignup/login-signup.module.ts
+++ src/app/modules/loginSignup/login-signup.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TsFormsModule } from '@townscript/elements';
 import { RecaptchaModule } from 'ng-recaptcha';
-import { HttpClientModule } from '@angular/common/http';
 import { MatRippleModule, MatSnackBarModule, MatInputModule, MatTooltipModule, MatProgressSpinnerModule } from '@angular/material';
 import { TsLoginSignupComponent } from './ts-login-signup/ts-login-signup.component';
 import { UserService } from '../../shared/services/user-service';
@@ -21,7 +20,6 @@ import { ConfirmationSVGComponent } from './ts-login-signup/confirmation-svg/con
     TsFormsModule,
     ReactiveFormsModule,
     RecaptchaModule,
-    HttpClientModule,
     MatRippleModule,
     MatSnackBarModule,
     MatInputModule,
