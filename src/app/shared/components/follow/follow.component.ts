25822db5dff329ce3adcac5b12f356735d4cd8ba
sumedhmohaneTS <sumedh.mohane@townscript.com>
2019-12-05 17:47:28 +0530

    imports fix

--- src/app/shared/components/follow/follow.component.ts
+++ src/app/shared/components/follow/follow.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { LoginModalComponent } from '../../../modules/loginSignup/ts-login-signup/login-modal/login-modal.component';
 import { MatDialogConfig, MatDialog } from '@angular/material';
 import { FollowService } from '../../services/follow.service';
