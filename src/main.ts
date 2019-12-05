8a1d29f2ece1f506af63c25935da3f24c38f40b2
sidharth6323 <sidharth6323@gmail.com>
2019-12-05 13:57:25 +0530

    fixed login signup

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,9 @@ if (environment.production) {
 }
 config.baseUrl = "https://www.tsdugout.in/";
 config.betaHostName = "beta.tsdugout.in/";
-config.token = "eyJhbGciOiJIUzUxMiJ9.eyJST0xFIjoiUk9MRV9DTElFTlQiLCJzdWIiOiJhcGlAdG93bnNjcmlwdC5jb20iLCJhdWRpZW5jZSI6IndlYiIsImNyZWF0ZWQiOjE1NjUyNTA4ODc2MDUsIlVTRVJfSUQiOjAsImV4cCI6MTU3MzAyNjg4N30.gfDC_wGGN05zCxhLKRm2uY_QjypCiz5qfwm7U0PqkIrywoDGuGcgDa9d1Vo9ftprpZ78A62HY-w4kjfiLOKC7g";
+config.token = 'eyJhbGciOiJIUzUxMiJ9.eyJST0xFIjoiUk9MRV9DTElFTlQiLCJzdWIiOiJhcGlAdG93bnNjcmlwdC5jb20iLCJhdWRpZW5jZSI6Indl' +
+  'YiIsImNyZWF0ZWQiOjE1NzIyNDkyMjI0MjksIlVTRVJfSUQiOjAsImV4cCI6MTU4MDAyNTIyMn0.HXRdlpKQG-s' +
+  'jkgI2r6thpUhtMQFUva-ahgDu4ETd51sl4EB_dzTYuBgp5a1k8Nv9IvK5eQsBQN3ppFhtjesmbg';
 config.algoliaIndexName = "tsTesting"
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
