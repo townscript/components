2e144a7a74f3b2adb11ec2298e0a3ac6fb07e09f
sidharth6323 <sidharth6323@gmail.com>
2019-12-16 16:44:06 +0530

    Update cookie.service.ts

--- src/app/core/cookie.service.ts
+++ src/app/core/cookie.service.ts
@@ -1,20 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, PLATFORM_ID, InjectionToken } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable()
 export class CookieService {
 
-  constructor() { }
+  constructor(@Inject(PLATFORM_ID) private platformId: InjectionToken<Object>, ) { }
 
   public getCookie(name: string): string | null {
-    const ca: Array<string> = document.cookie.split(';');
-    const caLen: number = ca.length;
-    const cookieName = `${name}=`;
-    let c: string;
+    if (isPlatformBrowser(this.platformId)) {
+      const ca: Array<string> = document.cookie.split(';');
+      const caLen: number = ca.length;
+      const cookieName = `${name}=`;
+      let c: string;
 
-    for (let i = 0; i < caLen; i += 1) {
-      c = ca[i].replace(/^\s+/g, '');
-      if (c.indexOf(cookieName) === 0) {
-        return c.substring(cookieName.length, c.length);
+      for (let i = 0; i < caLen; i += 1) {
+        c = ca[i].replace(/^\s+/g, '');
+        if (c.indexOf(cookieName) === 0) {
+          return c.substring(cookieName.length, c.length);
+        }
       }
     }
     return null;
@@ -25,11 +28,13 @@ export class CookieService {
   }
 
   public setCookie = (name: string, value: string, expireDays: number, path: string = ''): void => {
-    const d: Date = new Date();
-    d.setTime(d.getTime() + expireDays * 24 * 60 * 60 * 1000);
-    const expires: string = 'expires=' + d.toUTCString();
-    const host = '.' + window.location.host.split('.').splice(1).join('.');
-    document.cookie = name + '=' + value + '; ' + expires + (path.length > 0 ? '; path=' + path : '') + ';domain=' + host;
+    if (isPlatformBrowser(this.platformId)) {
+      const d: Date = new Date();
+      d.setTime(d.getTime() + expireDays * 24 * 60 * 60 * 1000);
+      const expires: string = 'expires=' + d.toUTCString();
+      const host = '.' + window.location.host.split('.').splice(1).join('.');
+      document.cookie = name + '=' + value + '; ' + expires + (path.length > 0 ? '; path=' + path : '') + ';domain=' + host;
+    }
   }
 
 }
