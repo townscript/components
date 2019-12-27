30c1cfcd31ba79610d17a7a9c8a041c9d0efed4a
sumedhmohaneTS <sumedh.mohane@townscript.com>
2019-12-27 20:53:12 +0530

    code refactoring

--- src/app/shared/pipes/ts-date-range.pipe.ts
+++ src/app/shared/pipes/ts-date-range.pipe.ts
@@ -1,5 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { DateTime } from 'luxon';
+import { UtilityService } from '../services/utilities.service';
 
 @Pipe({
     name: 'dateRange'
@@ -8,92 +9,11 @@ export class RangeDatePipe implements PipeTransform {
 
     days: any = {'SU':'Sun','MO': 'Mon','TU': 'Tue','WE': 'Wed','TH': 'Thu','FR': 'Fri','SA': 'Sat'};
 
-    deprecatedVsNewTimeZones : any = {
-    'Australia/ACT':'Australia/Sydney',
-    'Australia/LHI':'Australia/Lord_Howe',
-    'Australia/North':'Australia/Darwin',
-    'Australia/NSW':'Australia/Sydney',
-    'Australia/Queensland':'Australia/Brisbane',
-    'Australia/South':'Australia/Adelaide',
-    'Australia/Tasmania':'Australia/Hobart',
-    'Australia/Victoria':'Australia/Melbourne',
-    'Australia/West':'Australia/Perth',
-    'Brazil/Acre':'America/Rio_Branco',
-    'Brazil/DeNoronha':'America/Noronha',
-    'Brazil/East':'America/Sao_Paulo',
-    'Brazil/West':'America/Manaus',
-    'Canada/Atlantic':'America/Halifax',
-    'Canada/Central':'America/Winnipeg',
-    'Canada/Eastern':'America/Toronto',
-    'Canada/Mountain':'America/Edmonton',
-    'Canada/Newfoundland':'America/St_Johns',
-    'Canada/Pacific':'America/Vancouver',
-    'Canada/Saskatchewan':'America/Regina',
-    'Canada/Yukon':'America/Whitehorse',
-    'CET':'Europe/Paris.',
-    'Chile/Continental':'America/Santiago',
-    'Chile/EasterIsland':'Pacific/Easter',
-    'CST6CDT':'America/Chicago.',
-    'Cuba':'America/Havana',
-    'EET':'Europe/Sofia.',
-    'Egypt':'Africa/Cairo',
-    'Eire':'Europe/Dublin',
-    'EST':'America/Cancun.',
-    'EST5EDT':'America/New_York.',
-    'Etc/Greenwich':'Etc/GMT',
-    'Etc/Universal':'Etc/UTC',
-    'Etc/Zulu':'Etc/UTC',
-    'GB':'Europe/London',
-    'GB-Eire':'Europe/London',
-    'GMT+0':'Etc/GMT',
-    'GMT0':'Etc/GMT',
-    'GMT−0':'Etc/GMT',
-    'Greenwich':'Etc/GMT',
-    'Hongkong':'Asia/Hong_Kong',
-    'HST':'Pacific/Honolulu.',
-    'Iceland':'Atlantic/Reykjavik',
-    'Iran':'Asia/Tehran',
-    'Israel':'Asia/Jerusalem',
-    'Jamaica':'America/Jamaica',
-    'Japan':'Asia/Tokyo',
-    'Kwajalein':'Pacific/Kwajalein',
-    'Libya':'Africa/Tripoli',
-    'MET':'Europe/Paris.',
-    'Mexico/BajaNorte':'America/Tijuana',
-    'Mexico/BajaSur':'America/Mazatlan',
-    'Mexico/General':'America/Mexico_City',
-    'MST':'America/Phoenix.',
-    'MST7MDT':'America/Denver.',
-    'Navajo':'America/Denver',
-    'NZ':'Pacific/Auckland',
-    'NZ-CHAT':'Pacific/Chatham',
-    'Poland':'Europe/Warsaw',
-    'Portugal':'Europe/Lisbon',
-    'PRC':'Asia/Shanghai',
-    'PST8PDT':'America/Los_Angeles.',
-    'ROC':'Asia/Taipei',
-    'ROK':'Asia/Seoul',
-    'Singapore':'Asia/Singapore',
-    'Turkey':'Europe/Istanbul',
-    'UCT':'Etc/UCT',
-    'Universal':'Etc/UTC',
-    'US/Alaska':'America/Anchorage',
-    'US/Aleutian':'America/Adak',
-    'US/Arizona':'America/Phoenix',
-    'US/Central':'America/Chicago',
-    'US/Eastern':'America/New_York',
-    'US/East-Indiana':'America/Indiana/Indianapolis',
-    'US/Hawaii':'Pacific/Honolulu',
-    'US/Indiana-Starke':'America/Indiana/Knox',
-    'US/Michigan':'America/Detroit',
-    'US/Mountain':'America/Denver',
-    'US/Pacific':'America/Los_Angeles',
-    'US/Pacific-New':'America/Los_Angeles',
-    'US/Samoa':'Pacific/Pago_Pago',
-    'WET':'Europe/Lisbon.',
-    'W-SU':'Europe/Moscow',
-    'Zulu':'Etc/UTC'
-    };
+    deprecatedVsNewTimeZones : any = this.utilityService.deprecatedVsNewTimeZones;
+
+    constructor(private utilityService: UtilityService){
+
+    }
 
     transform = (rangeDates: any, eventTimeZone: any, isRecurrent?: any ,args?: any): any => {
 
