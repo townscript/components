e4896425cc3bf03e7bcbcb8ca3eb99a5d84374ae
sumedhmohaneTS <sumedh.mohane@townscript.com>
2020-01-03 13:41:52 +0530

    adding emitter for closecitysuggestions

--- src/app/modules/layout/components/ts-header/city-search-popup/city-search-popup.component.ts
+++ src/app/modules/layout/components/ts-header/city-search-popup/city-search-popup.component.ts
@@ -22,6 +22,7 @@ export class CitySearchPopupComponent implements OnInit, AfterViewInit {
     @Output() cityPopupActiveChange: EventEmitter<boolean> = new EventEmitter();
     @Input() popularPlaces: any;
     @Input() closeSuggestions: boolean = false;
+    @Output() closeSuggestionsChange: EventEmitter<boolean> = new EventEmitter();
 
     citySearchActive = true;
     placeSearchResults: any;
@@ -98,7 +99,12 @@ export class CitySearchPopupComponent implements OnInit, AfterViewInit {
         if (text != undefined && text.length > 0) {
             this.cityQueryChanged.next(text);
         }
-        this.closeSuggestions = false;
+        this.updateAndEmitCloseCitySuggestion(false);
+    }
+
+    updateAndEmitCloseCitySuggestion = (val: boolean): void => {
+      this.closeSuggestions = val;
+      this.closeSuggestionsChange.emit(this.closeSuggestions);
     }
 
     ngAfterViewInit() {
